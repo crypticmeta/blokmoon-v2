ba8bf7e2e2fbc04f5da15fc804524e3ade817e38
CrypticMeta <crypticmetadev@gmail.com>
2023-02-13 12:56:27 +0530

    added new service and arrows

--- src/components/Testimonials/index.tsx
+++ src/components/Testimonials/index.tsx
@@ -3,10 +3,13 @@ import Slider from "react-slick";
 
 function Testimonials() {
   const settings = {
-    className: "center",
+    dots: false,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    // className: "center",
     centerMode: true,
     infinite: true,
-    centerPadding: "60px",
+    // centerPadding: "60px",
     slidesToShow: 1,
     speed: 500,
     responsive: [
@@ -15,26 +18,26 @@ function Testimonials() {
         settings: {
           slidesToShow: 1,
           slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
+          // infinite: true,
+          // dots: true,
+        },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 2,
-          initialSlide: 2
-        }
+          initialSlide: 2,
+        },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
@@ -55,10 +58,10 @@ function Testimonials() {
           estimonials
         </h3>
       </div>
-      <div className=" px-10 lg:px-32 w-full   lg:w-10/12 md:w-9/12">
-        <div className="">
+      <div className="lg:px-32 w-full   lg:w-10/12 md:w-9/12">
+        <div className="w-full">
           <Slider {...settings}>
-            <div className="flex p-6 justify-center h-[400px] items-center">
+            <div className="relative flex p-2 md:p-6 justify-center h-[200px] md:h-[400px] items-center">
               <div className="flex justify-center  h-full">
                 <div className=" flex justify-center items-center w-full">
                   <img
@@ -68,7 +71,7 @@ function Testimonials() {
                 </div>
               </div>
             </div>
-            <div className="flex p-6 justify-center h-[400px] items-center">
+            <div className="relative flex p-2 md:p-6 justify-center h-[200px] md:h-[400px] items-center">
               <div className="flex justify-center  h-full">
                 <div className=" flex justify-center items-center w-full">
                   <img
@@ -78,7 +81,7 @@ function Testimonials() {
                 </div>
               </div>
             </div>
-            <div className="flex p-6 justify-center h-[400px] items-center">
+            <div className="relative flex p-2 md:p-6 justify-center h-[200px] md:h-[400px] items-center">
               <div className="flex justify-center  h-full">
                 <div className=" flex justify-center items-center w-full">
                   <img
@@ -88,7 +91,7 @@ function Testimonials() {
                 </div>
               </div>
             </div>
-            <div className="flex p-6 justify-center h-[400px] items-center">
+            <div className="relative flex p-2 md:p-6 justify-center h-[200px] md:h-[400px] items-center">
               <div className="flex justify-center  h-full">
                 <div className=" flex justify-center items-center w-full">
                   <img
@@ -104,5 +107,121 @@ function Testimonials() {
     </div>
   );
 }
+ function SamplePrevArrow(props) {
+   const { className, style, onClick } = props;
+
+   return (
+     <div
+       onClick={onClick}
+       style={{ zIndex: 200 }}
+       // style={{ ...style }}
+       className={
+         "arrows prev absolute top-0 bottom-0 left-0 z-50 flex cursor-pointer w-[100px] flex-col justify-center"
+       }
+     >
+       <svg viewBox="0 0 130 130" className="fill-brand_red">
+         <defs>
+           <filter
+             id="Ellipse_691"
+             x="0"
+             y="0"
+             width="130"
+             height="130"
+             filterUnits="userSpaceOnUse"
+           >
+             <feOffset dy="12" input="SourceAlpha" />
+             <feGaussianBlur stdDeviation="15" result="blur" />
+             <feFlood floodColor="#d82744" floodOpacity="0.502" />
+             <feComposite operator="in" in2="blur" />
+             <feComposite in="SourceGraphic" />
+           </filter>
+         </defs>
+         <g id="Group_228" data-name="Group 228" transform="translate(45 33)">
+           <g
+             transform="matrix(1, 0, 0, 1, -45, -33)"
+             filter="url(#Ellipse_691)"
+           >
+             <g
+               id="Ellipse_691-2"
+               data-name="Ellipse 691"
+               transform="translate(45 33)"
+               fill="#d82744"
+               stroke="#d82744"
+               strokeWidth="1"
+             >
+               <circle cx="20" cy="20" r="20" stroke="none" />
+               <circle cx="20" cy="20" r="19.5" fill="none" />
+             </g>
+           </g>
+           <path
+             id="Icon_awesome-arrow-down"
+             data-name="Icon awesome-arrow-down"
+             d="M14.57,9.21l.794-.794a.855.855,0,0,0,0-1.212L8.416.252A.855.855,0,0,0,7.2.252L.252,7.2a.855.855,0,0,0,0,1.212l.794.794A.859.859,0,0,0,2.273,9.2L6.378,4.887V15.164a.856.856,0,0,0,.858.858H8.38a.856.856,0,0,0,.858-.858V4.887L13.344,9.2A.853.853,0,0,0,14.57,9.21Z"
+             transform="translate(11.978 27.616) rotate(-90)"
+             fill="#fff"
+           />
+         </g>
+       </svg>
+     </div>
+   );
+ }
+ function SampleNextArrow(props) {
+   const { className, style, onClick } = props;
+
+   return (
+     <div
+       onClick={onClick}
+       style={{ zIndex: 200 }}
+       // style={{ ...style }}
+       className={
+         "arrows next absolute top-0 bottom-0 right-0 z-50 flex cursor-pointer w-[100px] flex-col justify-center"
+       }
+     >
+       <svg viewBox="0 0 130 130">
+         <defs>
+           <filter
+             id="Ellipse_691"
+             x="0"
+             y="0"
+             width="130"
+             height="130"
+             filterUnits="userSpaceOnUse"
+           >
+             <feOffset dy="12" input="SourceAlpha" />
+             <feGaussianBlur stdDeviation="15" result="blur" />
+             <feFlood floodColor="#d82744" floodOpacity="0.502" />
+             <feComposite operator="in" in2="blur" />
+             <feComposite in="SourceGraphic" />
+           </filter>
+         </defs>
+         <g id="Group_229" data-name="Group 229" transform="translate(45 33)">
+           <g
+             transform="matrix(1, 0, 0, 1, -45, -33)"
+             filter="url(#Ellipse_691)"
+           >
+             <g
+               id="Ellipse_691-2"
+               data-name="Ellipse 691"
+               transform="translate(45 33)"
+               fill="#d82744"
+               stroke="#d82744"
+               strokeWidth="1"
+             >
+               <circle cx="20" cy="20" r="20" stroke="none" />
+               <circle cx="20" cy="20" r="19.5" fill="none" />
+             </g>
+           </g>
+           <path
+             id="Icon_awesome-arrow-down"
+             data-name="Icon awesome-arrow-down"
+             d="M14.57,6.812l.794.794a.855.855,0,0,1,0,1.212L8.416,15.77a.855.855,0,0,1-1.212,0L.252,8.818a.855.855,0,0,1,0-1.212l.794-.794a.859.859,0,0,1,1.227.014l4.105,4.309V.858A.856.856,0,0,1,7.236,0H8.38a.856.856,0,0,1,.858.858V11.136l4.105-4.309A.853.853,0,0,1,14.57,6.812Z"
+             transform="translate(12 27.616) rotate(-90)"
+             fill="#fff"
+           />
+         </g>
+       </svg>
+     </div>
+   );
+ }
 
 export default Testimonials;
