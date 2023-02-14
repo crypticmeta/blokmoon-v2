1a917d7b605e372c9863ad7b7822cadab4d974de
CrypticMeta <crypticmetadev@gmail.com>
2023-02-14 14:04:26 +0530

    attempt1 to show inscription meta on sharing

--- src/pages/explorer/[id]/index.tsx
+++ src/pages/explorer/[id]/index.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useEffect, useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
+import Head from 'next/head'
 import Input from "../../../components/Explorer/Input";
 import Ordinal from "components/Explorer/Ordinal";
 import useDataStore from "stores/useDataStore";
@@ -39,6 +40,30 @@ function Id() {
   if (!data) {
     return (
       <div className="custom-container   h-screen">
+        <Head>
+          <title>Blokmoon Explorer | {data.title}</title>
+          <meta
+            name="description"
+            content={`Blokmoon BTC NFT explorer aims to help you search details related to your NFTs easily and in a readable format. This NFT is ${data.title} and has the ID ${data.id}`}
+          />
+          <meta
+            property="og:image"
+            content={`https://ordinals.com/content/${data.id}`}
+          />
+
+          {/* <!-- Twitter cards --> */}
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@blokmoon2021" />
+          <meta name="twitter:title" content="Blokmoon | Web3 Agency" />
+          <meta
+            name="twitter:description"
+            content={`Blokmoon BTC NFT explorer aims to help you search details related to your NFTs easily and in a readable format. This NFT is ${data.title} and has the ID ${data.id}`}
+          />
+          <meta
+            name="twitter:image"
+            content={`https://ordinals.com/content/${data.id}`}
+          />
+        </Head>
         <Input />
         {loading ? (
           <div className="text-brand_red h-50vh center">
